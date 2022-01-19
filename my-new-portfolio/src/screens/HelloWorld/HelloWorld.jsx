9aed3656050058580ea0ee8ddb97a685ae827460
SlayerT26 <456theochang@gmail.com>
2022-01-19 14:35:52 -0800

    final touch

--- my-new-portfolio/src/screens/HelloWorld/HelloWorld.jsx
+++ my-new-portfolio/src/screens/HelloWorld/HelloWorld.jsx
@@ -1,6 +1,6 @@
 import "./HelloWorld.css";
 import { Link } from "react-scroll";
-// import TypewriterEffect from "react-typewriter-effect";
+import { Typewriter } from "typewriting-react";
 
 function HelloWorld() {
   return (
@@ -12,24 +12,43 @@ function HelloWorld() {
             <div className="PythonDiv">
               <div className="Python">
                 {/* <TypewriterEffect text='print("Hello World")' typeSpeed={100} /> */}
-
-                <h1 className="Python">
+                <Typewriter
+                  className="TypeWriter"
+                  typingSpeed="100"
+                  words={[
+                    'print("Hello World")',
+                    'console.log("Hello World")',
+                    'cout << "Hello World" << endl',
+                    "put Hello World",
+                  ]}
+                  erasingSpeed="100"
+                  nextWordDelay="1000"
+                />
+                {/* <h1 className="Python">
                   <span className="PythonSpan1">print</span>(
                   <span className="PythonSpan2">"Hello World"</span>)
-                </h1>
+                </h1> */}
               </div>
             </div>
             <div className="JavaScriptDiv">
               <div className="JavaScript">
-                {/* <TypewriterEffect
-                  text='console.log("Hello World")'
-                  typeSpeed={70}
-                /> */}
-                <h1 className="JavaScript">
+                <Typewriter
+                  className="TypeWriter"
+                  typingSpeed="100"
+                  words={[
+                    'print("Hello World")',
+                    'console.log("Hello World")',
+                    'cout << "Hello World" << endl',
+                    "put Hello World",
+                  ]}
+                  erasingSpeed="100"
+                  nextWordDelay="1000"
+                />
+                {/* <h1 className="JavaScript">
                   <span className="JavaScriptSpan1">console</span>.
                   <span className="JavaScriptSpan2">log</span>(
                   <span className="JavaScriptSpan3">"Hello World"</span>)
-                </h1>
+                </h1> */}
               </div>
             </div>
           </div>
@@ -41,22 +60,41 @@ function HelloWorld() {
           <div className="HelloWorldDiv2">
             <div className="CDiv">
               <div className="C">
-                {/* <TypewriterEffect
-                  text='cout << "Hello World" << endl;'
-                  typeSpeed={70}
-                /> */}
-                <h1 className="C">
+                <Typewriter
+                  className="TypeWriter"
+                  typingSpeed="100"
+                  words={[
+                    'print("Hello World")',
+                    'console.log("Hello World")',
+                    'cout << "Hello World" << endl',
+                    "put Hello World",
+                  ]}
+                  erasingSpeed="100"
+                  nextWordDelay="1000"
+                />
+                {/* <h1 className="C">
                   cout &lt;&lt; "Hello World" &lt;&lt; endl;
-                </h1>
+                </h1> */}
               </div>
             </div>
             <div className="RubyDiv">
               <div className="Ruby">
-                {/* <TypewriterEffect text="put Hello World" typeSpeed={100} /> */}
-                <h1 className="Ruby">
+                <Typewriter
+                  className="TypeWriter"
+                  typingSpeed="100"
+                  words={[
+                    'print("Hello World")',
+                    'console.log("Hello World")',
+                    'cout << "Hello World" << endl',
+                    "put Hello World",
+                  ]}
+                  erasingSpeed="100"
+                  nextWordDelay="1000"
+                />
+                {/* <h1 className="Ruby">
                   <span className="RubySpan1">put </span>
                   <span className="RubySpan2"> Hello World</span>
-                </h1>
+                </h1> */}
               </div>
             </div>
           </div>
